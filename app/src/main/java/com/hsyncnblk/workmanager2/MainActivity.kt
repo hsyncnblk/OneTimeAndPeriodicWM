583341df69164964d4ace183f2cc5a813209587d
huseyin cinibulak <huseyincnblk@gmail.com>
2023-12-18 15:02:00 +0300

    OneTimeWorkRequest complated

--- app/src/main/java/com/hsyncnblk/workmanager2/MainActivity.kt
+++ app/src/main/java/com/hsyncnblk/workmanager2/MainActivity.kt
@@ -2,7 +2,15 @@ package com.hsyncnblk.workmanager2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.hsyncnblk.workmanager2.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             buttonYap.setOnClickListener {
+
+                val calismaKosulu = Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED).build()
+
+                val istek = OneTimeWorkRequestBuilder<MyWorker>()
+                    .setInitialDelay(10,TimeUnit.SECONDS) // 10 SANİYE GECİKMELİ
+                    .setConstraints(calismaKosulu) // internete bağlıysa
+                    .build()
+
+                WorkManager.getInstance(this@MainActivity).enqueue(istek)
+
+                WorkManager.getInstance(this@MainActivity).getWorkInfoByIdLiveData(istek.id)
+                    .observe(this@MainActivity){
+
+                        val durum= it.state.name
+                        Log.e("arkaplan işlem durumu ",durum)
+
+                    }
 
 
 
